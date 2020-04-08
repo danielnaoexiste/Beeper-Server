556c2f98c588808dbc1b5f8674bc056a6d40f6e4
Daniel <danieldenardo1@gmail.com>
2020-04-07 23:15:34 -0300

    Error Handling

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,7 +9,7 @@ router.post('/signup', async (req, res) => {
 	const { email, password } = req.body;
 	
 	if (!validator.isEmail(email)) return res.status(422).send({ error: "Invalid email!" });
-	if (password.length < 8) return res.status(422).send({ error: "Password is too short!" });
+	if (password.length < 8) return res.status(422).send({ error: "Password too short!" });
 	
 	try {
 		const user = new User({ email, password })
@@ -27,7 +27,7 @@ router.post('/signin', async (req, res) => {
 	if (!email || !password) return res.status(422).send({ error: 'Must provide email and password!' })
 	
 	const user = await User.findOne({ email });
-	if (!user) return res.status(422).send({ error: 'Invalid password or Email' })
+	if (!user) return res.status(422).send({ error: 'Invalid email or password!' })
 	
 	try {
 		await user.comparePassword(password);
@@ -35,7 +35,7 @@ router.post('/signin', async (req, res) => {
 		console.log(process.env.SECRET_KEY)
 		res.send({ token })
 	} catch(e) {
-		return res.status(422).send({ error: "Invalid password or Email" });
+		return res.status(422).send({ error: "Invalid email or password" });
 	}
 
 })
